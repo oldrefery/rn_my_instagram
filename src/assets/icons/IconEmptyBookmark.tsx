63c67c9c1b3a8a2401e4644006553ddfc62ad6cb
Rustem.Shakirov <Rustem.Shakirov@emea.merkleinc.com>
2023-07-05 14:59:30 +0200

    feat: add new svg icons

---
+++ src/assets/icons/IconEmptyBookmark.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Svg, {Path} from 'react-native-svg';
+import {IconSVGProps} from '../../types/common';
+export const IconEmptyBookmark = ({
+  size = 24,
+  color = 'black',
+}: IconSVGProps) => (
+  <Svg
+    fill="none"
+    stroke={color}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}>
+    <Path d="m19 21-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+  </Svg>
+);
